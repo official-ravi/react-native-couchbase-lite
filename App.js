747b91e68cd01802fbbc8e545bf230b14c3f53b1
Kirill Lebedev <echo.bjornd@gmail.com>
2018-04-01 22:42:16 +0300

    Added native base

--- App.js
+++ App.js
@@ -4,13 +4,9 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, { Component } from 'react'
+import { Container, Header, Title, Content, Button, Left, Right, Body, Icon, Text } from 'native-base'
+import { StyleSheet, View } from 'react-native'
 
 import CouchbaseLite, { cblProvider } from 'react-native-cbl';
 
@@ -28,14 +24,7 @@ CouchbaseLite.openDb('odygos', false).then( () =>
   CouchbaseLite.updateDocument( '_design/main', { views } )
 )
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
+type Props = {}
 
 @cblProvider( props => ({
   notes: {
@@ -45,36 +34,24 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='menu' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Header</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content>
+          <Text>
+            This is Content Section
+          </Text>
+        </Content>
+      </Container>
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
