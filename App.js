919063ef556a65189034e665da282e86bf02f386
Ravi Kumar <ravi.kumar@smartdatainc.net>
2018-10-05 12:28:23 +0530

    local configuration to setup couchbase server with sync

--- App.js
+++ App.js
@@ -1,48 +1,51 @@
-import React, { Component } from 'react'
-import { StyleSheet, View, Platform } from 'react-native'
-import { StackNavigator } from 'react-navigation'
-import HomeScreen from './src/home-screen'
-import NoteScreen from './src/note-screen'
-import NoteModalScreen from './src/note-modal-screen'
-import CouchbaseLite, { CBLConnection, CBLConnector } from 'react-native-cbl'
+import React, { Component } from "react";
+import { StyleSheet, View, Platform } from "react-native";
+import { StackNavigator } from "react-navigation";
+import HomeScreen from "./src/home-screen";
+import NoteScreen from "./src/note-screen";
+import NoteModalScreen from "./src/note-modal-screen";
+import CouchbaseLite, { CBLConnection, CBLConnector } from "react-native-cbl";
 
 const views = {
   notes: {
     map: function(doc) {
-      if (doc.docType == 'note') {
-        emit(doc.createdTime, null)
+      if (doc.docType == "note") {
+        emit(doc.createdTime, null);
       }
-    }.toString(),
+    }.toString()
+  }
+};
+const syncGatewayHost = Platform.OS == "android" ? "10.0.2.2" : "localhost";
+const dbName = "notes";
+const syncGatewayUrl = `http://${syncGatewayHost}:4984/${dbName}/`;
+console.log(syncGatewayUrl, "syncGatewayUrlsyncGatewayUrlsyncGatewayUrl");
+const StackNavigatorApp = StackNavigator(
+  {
+    Root: {
+      screen: StackNavigator({
+        Home: {
+          screen: HomeScreen
+        },
+        Note: {
+          screen: NoteScreen
+        }
+      })
+    },
+    NoteModal: {
+      screen: NoteModalScreen
+    }
   },
-}
-const syncGatewayHost = Platform.OS == 'android' ? '10.0.2.2' : 'localhost'
-const dbName = 'notes'
-const syncGatewayUrl = `http://${syncGatewayHost}:4984/${dbName}/`
-
-const StackNavigatorApp = StackNavigator({
-  Root: {
-    screen: StackNavigator({
-      Home: {
-        screen: HomeScreen,
-      },
-      Note: {
-        screen: NoteScreen,
-      },
-    })
-  },
-  NoteModal: {
-    screen: NoteModalScreen,
-  },
-},{
-  mode: 'modal',
-  headerMode: 'none',
-})
+  {
+    mode: "modal",
+    headerMode: "none"
+  }
+);
 
 const cblConnection = new CBLConnection({
   dbName,
   syncGatewayUrl,
-  views,
-})
+  views
+});
 
 export default class App extends React.Component {
   render() {
@@ -50,6 +53,6 @@ export default class App extends React.Component {
       <CBLConnector connection={cblConnection}>
         <StackNavigatorApp />
       </CBLConnector>
-    )
+    );
   }
 }
